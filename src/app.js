4c1c3b1b93ebdabb23d91adbe441d2d4c65f254c
Sarah Kate Davidson-Catalano <sarahkate@Sarahs-MacBook-Air.local>
2022-02-27 23:37:21 -0500

    started unit conversion

--- src/app.js
+++ src/app.js
@@ -77,3 +77,19 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#city-search");
 form.addEventListener("submit", handleSubmit);
+
+
+function displayFarenheitTemperature(event); {
+    event.preventDefault();
+let farenheitTemperature = (14 * 9)/5 + 32;
+alert(farenheitTemperature);
+let temperatureElement = document.querySelector("#current-temp");
+temperatureElement.innerHTML = Math.round(farenheitTemperature);
+}
+
+let form = document.querySelector("#city-search");
+form.addEventListener("submit", handleSubmit);
+
+let farenheitLink = document.querySelector("#farenheit-link")
+farenheitLink.addEventListener("click", displayFarenheitTemperature);
+
